d563c215b9eb6f23b455a45dc5719a7c28baf1d6
Nicolas <nicolasd@berkeley.edu>
2024-05-22 22:17:40 -0700

    finished part 1: set up business, business list, search bar, and formatted with css

---
+++ src/BusinessList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Business from './Business';
+import './BusinessList.css';
+
+function BusinessList() {
+    return (
+        <div className="BusinessList">
+          <Business />
+          <Business />
+          <Business />
+          <Business />
+          <Business />
+          <Business />
+          <Business />
+          <Business />
+        </div>
+    );
+}
+
+export default BusinessList;
